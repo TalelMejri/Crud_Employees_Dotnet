5a1143b4165c7fef853a7f4cadf46955c484ece4
talel <talelmejri8@gmail.com>
2023-06-22 21:49:00 +0100

    add controller

---
+++ Employees/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Employees.Models; // app > models to use dbcontext
+namespace Employees.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        HRDataBaseContact dbContext=new HRDataBaseContact();
+        public IActionResult Index()
+        {
+            List<Models.Employees> employee=dbContext.Employees.ToList(); // because i used name app some name model (Models.Employees) to fix it
+            return View(employee);
+        }
+    }
+}
