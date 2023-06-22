bb0c72ed037220e80c0afc082eb415d60aa200a0
talel <talelmejri8@gmail.com>
2023-06-22 21:48:38 +0100

    add migraion

---
+++ Employees/Migrations/20230622162652_InitialMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Employees.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "Varchar(150)", nullable: false),
+                    DepartmentAbbr = table.Column<string>(type: "Varchar(5)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                schema: "dbo",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeNumber = table.Column<string>(type: "Varchar(5)", maxLength: 5, nullable: false),
+                    EmployeeName = table.Column<string>(type: "Varchar(5)", maxLength: 5, nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HiringBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GrossSalary = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    NetSalary = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "DepartmentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                schema: "dbo",
+                table: "Employee",
+                column: "DepartmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
